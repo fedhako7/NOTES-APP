f6086522be39f7c5ed3e8fedc2074944a1129dbb
sabaif-s <sebaifmuhammed33@gmail.com>
2025-01-30 13:39:31 +0300

    adding get by id and post

--- NotesApp/Controllers/NotesController.cs
+++ NotesApp/Controllers/NotesController.cs
@@ -19,4 +19,28 @@ namespace NotesAppAPI.Controllers
       
 
     }
+            [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> Get(string id)
+        {
+            var note = await _notesService.GetByIdAsync(id);
+
+            if (note is null)
+                return NotFound();
+
+            return note;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Note newNote)
+        {
+            // Validate the model state
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);  // Return a bad request if validation fails
+            }
+
+            await _notesService.CreateAsync(newNote);
+            return CreatedAtAction(nameof(Get), new { id = newNote.Id }, newNote);
+        }
+
 }
